62be8cdb7b36821b58ed69d4c1c30ed4ff680ac7
Jacob Hansen <jacobhansen@abigails-air-3.mynetworksettings.com>
2024-03-24 16:41:43 -0400

    Updated sheets.py (not sure if working)

--- sheets.py
+++ sheets.py
@@ -41,7 +41,7 @@ def main():
           "credentials.json", SCOPES
       )
 
-    creds = flow.run_console()
+    # creds = flow.run_console()
     creds = flow.run_local_server(port=0)
     # Save the credentials for the next run
     with open("token.json", "w") as token:
